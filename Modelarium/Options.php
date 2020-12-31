524bd5fa623b7ffa08357d1f83a7de49ee6002be
Bruno <brunobg@gmail.com>
2020-12-31 14:52:41 -0300

    options for modelarium

---
+++ Modelarium/Options.php
@@ -0,0 +1,86 @@
+<?php declare(strict_types=1);
+
+namespace Modelarium;
+
+use ArrayAccess;
+
+use function Safe\file_get_contents;
+use function Safe\getcwd;
+use function Safe\json_decode;
+use function Safe\substr;
+
+class Options
+{
+    /**
+     * @var string
+     */
+    public $basePath = null;
+
+    /**
+     *
+     * @var array
+     */
+    public $options = [];
+
+    public function __construct(string $basePath = null)
+    {
+        $this->basePath = $basePath;
+        $this->options = $this->loadOptions();
+    }
+
+    /**
+     * Gets a value
+     *
+     * @param string $section
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    public function getOption(string $section, string $key, $default = null)
+    {
+        return $this->options[$section][$key] ?? $default;
+    }
+
+    public function getSection(string $section): array
+    {
+        return $this->options[$section] ?? [];
+    }
+
+    public function setSectionDefaults(string $section, array $defaults): self
+    {
+        if (!array_key_exists($section, $this->options)) {
+            $this->options[$section] = $defaults;
+        } else {
+            $this->options[$section] = array_merge($defaults, $this->options[$section]);
+        }
+        return $this;
+    }
+
+    /**
+     * Returns the base path where we expect to find the options
+     *
+     * @return string
+     */
+    protected function getBasePath(): string
+    {
+        if ($this->basePath) {
+            return $this->basePath;
+        }
+        // assume we're in vendor/
+        $path = __DIR__;
+        $start = mb_strpos('/vendor/', $path);
+        if ($start !== false) {
+            return substr($path, $start);
+        }
+        return getcwd();
+    }
+
+    protected function loadOptions(): array
+    {
+        $filename = $this->getBasePath() . '/modelarium.json';
+        if (!file_exists($filename)) {
+            return [];
+        }
+        return json_decode(file_get_contents($filename), true);
+    }
+}
